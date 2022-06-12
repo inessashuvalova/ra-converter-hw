6a7c388e5457ffb44608446c39c17ba422260b64
Inessa Shuvalova <inessa.shuvalova@mail.ru>
2022-06-13 01:24:58 +0300

    initial

---
+++ ra-converter/src/App.js
@@ -0,0 +1,12 @@
+import HexRgbConverter from './components/HexRgbConverter/HexRgbConverter'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HexRgbConverter/>
+    </div>
+  );
+}
+
+export default App;
